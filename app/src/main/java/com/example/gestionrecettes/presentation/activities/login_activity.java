9d7dd55d25afa3cce714edbe172cad07aeddc487
Mehdi Kaalat <mehdikaalat@gmail.com>
2024-05-29 14:42:49 +0100

    Add data

--- app/src/main/java/com/example/gestionrecettes/presentation/activities/login_activity.java
+++ app/src/main/java/com/example/gestionrecettes/presentation/activities/login_activity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -27,6 +31,8 @@ import com.example.gestionrecettes.presentation.viewmodels.SignupViewModel;
 import com.example.gestionrecettes.presentation.viewmodels.SignupViewModelFactory;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class login_activity extends AppCompatActivity {
 
@@ -56,7 +62,7 @@ public class login_activity extends AppCompatActivity {
 
         LoginViewModelFactory factory = new LoginViewModelFactory(userRepository);
         loginViewModel = new ViewModelProvider(this, factory).get(LoginViewModel.class);
-
+        createUser();
         binding.buttonLogin.setOnClickListener(v -> login());
 
         binding.goSignupPage.setOnClickListener(v -> navigateToSignUp());
@@ -98,5 +104,53 @@ public class login_activity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt("USER_ID", userId);
         editor.apply();
+    }
+    private void createUser(){
+        selectedImageBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.mehdi);
+        String name = "mehdi";
+        String email = "mehdi@gmail.com";
+        String password = "mehdi";
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        selectedImageBitmap.compress(Bitmap.CompressFormat.WEBP, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        signupViewModel.register(imageBytes ,name, email, password);
+        saveRecipe();
+        saveRecipe();
+    }
+    private void saveRecipe() {
+        int userId = 1;
+        String title = "Traditional Spare Ribs Baked";
+        String description = "Preheat the oven to 150°C (300°F).\n" +
+                "Season the spare ribs with salt, pepper, and your favorite dry rub.\n" +
+                "Place the ribs on a baking sheet lined with foil.\n" +
+                "Cover the ribs with another layer of foil and seal the edges.\n" +
+                "Bake in the preheated oven for 2 hours.\n" +
+                "Remove the top foil layer and brush the ribs with barbecue sauce.\n" +
+                "Increase the oven temperature to 200°C (400°F) and bake for an additional 30 minutes.\n" +
+                "Let the ribs rest for 10 minutes before serving.";
+        int cookingTime = 150;
+        String category = "Main Course";
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.img1);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+
+        List<Ingredient> ingredients = new ArrayList<>();
+        List<String> quantities = new ArrayList<>();
+
+        ingredients.add(new Ingredient("Spare Ribs"));
+        quantities.add("1000");
+        ingredients.add(new Ingredient("Salt"));
+        quantities.add("10");
+        ingredients.add(new Ingredient("Pepper"));
+        quantities.add("5");
+        ingredients.add(new Ingredient("Dry Rub"));
+        quantities.add("30");
+        ingredients.add(new Ingredient("Barbecue Sauce"));
+        quantities.add("200");
+
+        Recipe recipe = new Recipe(userId, title, description, imageBytes, cookingTime, category);
+        addRecipeViewModel.insertRecipeWithIngredients(recipe, ingredients.toArray(new Ingredient[0]), quantities.toArray(new String[0]));
     }
 }
